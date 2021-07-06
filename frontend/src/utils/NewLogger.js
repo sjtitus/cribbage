608ffe9996288893ea1532a8361618044c15c9ef
Stephen Titus <sjtitus@alumni.duke.edu>
2021-07-05 21:24:46 -0400

    added new logger to front end (wire up later)

---
+++ frontend/src/utils/NewLogger.js
@@ -0,0 +1,110 @@
+/* _____________________________________________________________________________
+    Logger
+    Simple logger using winston.
+  ______________________________________________________________________________
+*/
+import httpContext from 'express-http-context';
+import winston from 'winston';
+import Config from '../../config/Config.js';
+
+const logFileName = Config.logFilePath;
+
+const ModuleFormat = winston.format(info => {
+    if (!('module' in info)) {
+        info.module = "main";
+    }
+    info.module = info.module.substr(0,20);
+    info.module = `[${info.module}]`;
+    info.module = info.module.padStart(22);
+
+    const reqid = httpContext.get('reqid');
+    info.reqid = (reqid) ? reqid.substr(0,8):'========';
+    info.message = `[${info.reqid}] ${info.message}`;
+    return info;
+});
+
+// Create/Get a logger for a specific code module.
+// Logging level for the module set in config file Config.logLevels
+export function GetModuleLogger(moduleName) {
+    if (!winston.loggers.has(moduleName)) {
+        const level = (Config.logLevels[moduleName]) ? Config.logLevels[moduleName]:Config.logLevelDefault;
+        winston.loggers.add(moduleName, {
+            format: winston.format.combine(
+                ModuleFormat(),
+                winston.format.timestamp(),
+                winston.format.align(),
+                winston.format.printf(info => `${info.timestamp} ${info.module} ${info.level} ${info.message}`)
+            ),
+            level: level, 
+            transports: [
+                new winston.transports.File({ 
+                    filename: logFileName, 
+                    handleExceptions: true
+                }),
+                new winston.transports.Console({
+                    format: winston.format.combine(
+                        winston.format.colorize(),
+                        winston.format.printf(info => `${info.timestamp} ${info.module} ${info.level} ${info.message}`)
+                    ),
+                    handleExceptions: true
+                }),
+            ],
+        });
+    }
+    return winston.loggers.get(moduleName).child({module:moduleName});
+}
+
+
+/*
+
+//const APIlogFileName = 'access.log';
+//const APIlogLevel = 'debug';
+
+const Logger = winston.createLogger({
+    format: winston.format.combine(
+        ModuleFormat(),
+        winston.format.timestamp(),
+        winston.format.align(),
+        winston.format.printf(info => `${info.timestamp} ${info.module} ${info.level} ${info.message}`)
+    ),
+    level: logLevel,
+    transports: [
+        new winston.transports.File({ 
+            filename: logFileName, 
+            //level: logLevel,
+            handleExceptions: true
+        }),
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.printf(info => `${info.timestamp} ${info.module} ${info.level} ${info.message}`)
+            ),
+            //level: logLevel,
+            handleExceptions: true
+        }),
+    ],
+});
+export const APILogger = winston.createLogger({
+    format: winston.format.combine(
+        ModuleFormat(),
+        winston.format.timestamp(),
+        winston.format.align(),
+        winston.format.printf(info => `${info.timestamp} ${info.module}\t${info.level} ${info.message}`)
+    ),
+    level: logLevel,
+    transports: [
+        new winston.transports.File({ 
+            filename: APIlogFileName, 
+            level: APIlogLevel,
+            handleExceptions: true
+        }),
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.printf(info => `${info.timestamp} ${info.module} ${info.level} ${info.message}`)
+            ),
+            handleExceptions: true
+        }),
+    ],
+});
+*/
