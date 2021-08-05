25761591a747cf9cc2c0871863972e4cdc1c7ece
sjtitus <sjtitus@alumni.duke.edu>
2021-08-05 19:28:13 -0400

    mods when macbook was in shop

--- test/api/api.js
+++ test/api/api.js
@@ -1,33 +1,31 @@
 /*_____________________________________________________________________________
-    api 
-    Module for calling backend API. Uses axios. 
-         
-    Response
-          statusCode
-          statusText
-          data
-          config
-          request
-          response
-          error
-          errorText
+    api
+    Module for calling backend API. Uses axios.
+
+    Success Response
+        statusCode
+        statusText
+        data
+        config
+        request
+        response
+        error
+        errorText
+
+    Error Response
+        code (string)
+        message
+        config
+        request
+        response
+        stack
+        toJSON()
   _____________________________________________________________________________
 */
 import axios from 'axios';
 import { strict as assert } from 'assert';
 import {GetModuleLogger} from '../util/Logger.js';
 const log = GetModuleLogger('api');
-        
-const emptyResponse = { 
-          statusCode: null, 
-          statusText: null,
-          data: null, 
-          config: null, 
-          request: null, 
-          response: null, 
-          error: null,
-          errorText: null
-};
 
 export class apiRequest {
 
@@ -41,88 +39,45 @@ export class apiRequest {
     // Execute the request
     async execute(onPending=null, onSuccess=null, onError=null, onCancel=null) {
         log.debug(`apiRequest: execute: params=${JSON.stringify(this.params)}`);
-        const out = { ...emptyResponse }; 
+        const out = {};
         try {
             onPending && onPending();
             const axiosResponse = await this.client.api.request(this.params);
-            // `data` is the response that was provided by the server
-            // `status` is the HTTP status code from the server response
-            // `statusText` is the HTTP status message from the server response
-            // `headers` the HTTP headers; e.g. `response.headers['content-type']`
-            // `config` is the config that was provided to `axios` for the request
-            // `request` is the request that generated this response
-            log.debug(`apiRequest: success`); 
+            // Axios response
+            //  `data` is the response that was provided by the server
+            //  `status` is the HTTP status code from the server response
+            //  `statusText` is the HTTP status message from the server response
+            //  `headers` the HTTP headers; e.g. `response.headers['content-type']`
+            //  `config` is the config that was provided to `axios` for the request
+            //  `request` is the request that generated this response
+            log.debug(`apiRequest: success`);
             out.statusCode = axiosResponse.status;
             out.statusText = axiosResponse.statusText;
             out.data = axiosResponse.data;
             out.config = axiosResponse.config;
             out.request = axiosResponse.request;
             out.response = axiosResponse;
-            out.error = false;
-            out.errorText = null;
-
+            onSuccess && onSuccess(out);
         }
         catch (err) {
-                out.state = 'error';
-                out.error.message = err.message;
-                out.error.config = err.config;
-                out.request = err.request || null;
-                out.response = err.response || null;
-                if (err.response) {
-                    log.error(`apiRequest: (response received): error: ${err.message}`); 
-                    out.data = err.response.data; 
-                    out.status = err.response.status;
-                }
-                else if (err.request) {
-                    log.error(`apiRequest: (no response received): error: ${err.message}`); 
-                }
-                else {
-                    log.error(`apiRequest: (no request made): error: ${err.message}`); 
-                }
+            if (err.response) {
+              log.error(`apiRequest: error (response received): ${err.message}`);
             }
-            finally {
-              return out;
+            else if (err.request) {
+              log.error(`apiRequest: error (no response received): ${err.message}`);
             }
-        }
-        else {
-            log.debug(`apiRequest: execute (callback): params=${JSON.stringify(this.params)}`);
-            try {
-                onLoading && onLoading();
-                const axiosResponse = await this.client.api.request(this.params);
-                log.debug(`apiRequest: success`); 
-                out.state = 'success';
-                out.status = axiosResponse.status;
-                out.request = axiosResponse.request;
-                out.response = axiosResponse;
-                out.data = axiosResponse.data;
-                onSuccess && onSuccess(out);
+            else {
+              log.error(`apiRequest: error (no request made): ${err.message}`);
             }
-            catch (err) {
-                out.state = 'error';
-                out.error.message = err.message;
-                out.error.config = err.config;
-                out.request = err.request || null;
-                out.response = err.response || null;
-                if (err.response) {
-                    log.error(`apiRequest: (response received): error: ${err.message}`); 
-                    out.data = err.response.data; 
-                    out.status = err.response.status;
-                }
-                else if (err.request) {
-                    log.error(`apiRequest: (no response received): error: ${err.message}`); 
-                }
-                else {
-                    log.error(`apiRequest: (no request made): error: ${err.message}`); 
-                }
-                onError && onError(out);
+            if (onError) {
+              onError(err);
             }
-            finally {
-                log.debug(`apiRequest: finished`); 
+            else {
+              throw err;
             }
         }
     }
-
-} 
+}
 
 export class apiClient {
 
@@ -133,188 +88,7 @@ export class apiClient {
         this.api.defaults.headers.post['Content-Type'] = 'application/json';
     }
 
-
 }
-
-/*_________________________________________________________________________________________________
-      DOCS FOR THE CONFIG
-      
-      // `url` is the server URL that will be used for the request
-      url: '/user',
-
-      // `method` is the request method to be used when making the request
-      method: 'get', // default
-
-      // `baseURL` will be prepended to `url` unless `url` is absolute.
-      // It can be convenient to set `baseURL` for an instance of axios to pass relative URLs
-      // to methods of that instance.
-      baseURL: 'https://some-domain.com/api/',
-
-      // `transformRequest` allows changes to the request data before it is sent to the server
-      // This is only applicable for request methods 'PUT', 'POST', 'PATCH' and 'DELETE'
-      // The last function in the array must return a string or an instance of Buffer, ArrayBuffer,
-      // FormData or Stream
-      // You may modify the headers object.
-      transformRequest: [function (data, headers) {
-        // Do whatever you want to transform the data
-
-        return data;
-      }],
-
-      // `transformResponse` allows changes to the response data to be made before
-      // it is passed to then/catch
-      transformResponse: [function (data) {
-        // Do whatever you want to transform the data
-
-        return data;
-      }],
-
-      // `headers` are custom headers to be sent
-      headers: {'X-Requested-With': 'XMLHttpRequest'},
-
-      // `params` are the URL parameters to be sent with the request
-      // Must be a plain object or a URLSearchParams object
-      // NOTE: params that are null or undefined are not rendered in the URL.
-      params: {
-        ID: 12345
-      },
-
-      // `paramsSerializer` is an optional function in charge of serializing `params`
-      // (e.g. https://www.npmjs.com/package/qs, http://api.jquery.com/jquery.param/)
-      paramsSerializer: function (params) {
-        return Qs.stringify(params, {arrayFormat: 'brackets'})
-      },
-
-      // `data` is the data to be sent as the request body
-      // Only applicable for request methods 'PUT', 'POST', 'DELETE , and 'PATCH'
-      // When no `transformRequest` is set, must be of one of the following types:
-      // - string, plain object, ArrayBuffer, ArrayBufferView, URLSearchParams
-      // - Browser only: FormData, File, Blob
-      // - Node only: Stream, Buffer
-      data: {
-        firstName: 'Fred'
-      },
-      
-      // syntax alternative to send data into the body
-      // method post
-      // only the value is sent, not the key
-      data: 'Country=Brasil&City=Belo Horizonte',
-
-      // `timeout` specifies the number of milliseconds before the request times out.
-      // If the request takes longer than `timeout`, the request will be aborted.
-      timeout: 1000, // default is `0` (no timeout)
-
-      // `withCredentials` indicates whether or not cross-site Access-Control requests
-      // should be made using credentials
-      withCredentials: false, // default
-
-      // `adapter` allows custom handling of requests which makes testing easier.
-      // Return a promise and supply a valid response (see lib/adapters/README.md).
-      adapter: function (config) {
-         ... 
-      },
-
-      // `auth` indicates that HTTP Basic auth should be used, and supplies credentials.
-      // This will set an `Authorization` header, overwriting any existing
-      // `Authorization` custom headers you have set using `headers`.
-      // Please note that only HTTP Basic auth is configurable through this parameter.
-      // For Bearer tokens and such, use `Authorization` custom headers instead.
-      auth: {
-        username: 'janedoe',
-        password: 's00pers3cret'
-      },
-
-      // `responseType` indicates the type of data that the server will respond with
-      // options are: 'arraybuffer', 'document', 'json', 'text', 'stream'
-      //   browser only: 'blob'
-      responseType: 'json', // default
-
-      // `responseEncoding` indicates encoding to use for decoding responses (Node.js only)
-      // Note: Ignored for `responseType` of 'stream' or client-side requests
-      responseEncoding: 'utf8', // default
-
-      // `xsrfCookieName` is the name of the cookie to use as a value for xsrf token
-      xsrfCookieName: 'XSRF-TOKEN', // default
-
-      // `xsrfHeaderName` is the name of the http header that carries the xsrf token value
-      xsrfHeaderName: 'X-XSRF-TOKEN', // default
-
-      // `onUploadProgress` allows handling of progress events for uploads
-      // browser only
-      onUploadProgress: function (progressEvent) {
-        // Do whatever you want with the native progress event
-      },
-
-      // `onDownloadProgress` allows handling of progress events for downloads
-      // browser only
-      onDownloadProgress: function (progressEvent) {
-        // Do whatever you want with the native progress event
-      },
-
-      // `maxContentLength` defines the max size of the http response content in bytes allowed in node.js
-      maxContentLength: 2000,
-
-      // `maxBodyLength` (Node only option) defines the max size of the http request content in bytes allowed
-      maxBodyLength: 2000,
-
-      // `validateStatus` defines whether to resolve or reject the promise for a given
-      // HTTP response status code. If `validateStatus` returns `true` (or is set to `null`
-      // or `undefined`), the promise will be resolved; otherwise, the promise will be
-      // rejected.
-      validateStatus: function (status) {
-        return status >= 200 && status < 300; // default
-      },
-
-      // `maxRedirects` defines the maximum number of redirects to follow in node.js.
-      // If set to 0, no redirects will be followed.
-      maxRedirects: 5, // default
-
-      // `socketPath` defines a UNIX Socket to be used in node.js.
-      // e.g. '/var/run/docker.sock' to send requests to the docker daemon.
-      // Only either `socketPath` or `proxy` can be specified.
-      // If both are specified, `socketPath` is used.
-      socketPath: null, // default
-
-      // `httpAgent` and `httpsAgent` define a custom agent to be used when performing http
-      // and https requests, respectively, in node.js. This allows options to be added like
-      // `keepAlive` that are not enabled by default.
-      httpAgent: new http.Agent({ keepAlive: true }),
-      httpsAgent: new https.Agent({ keepAlive: true }),
-
-      // `proxy` defines the hostname, port, and protocol of the proxy server.
-      // You can also define your proxy using the conventional `http_proxy` and
-      // `https_proxy` environment variables. If you are using environment variables
-      // for your proxy configuration, you can also define a `no_proxy` environment
-      // variable as a comma-separated list of domains that should not be proxied.
-      // Use `false` to disable proxies, ignoring environment variables.
-      // `auth` indicates that HTTP Basic auth should be used to connect to the proxy, and
-      // supplies credentials.
-      // This will set an `Proxy-Authorization` header, overwriting any existing
-      // `Proxy-Authorization` custom headers you have set using `headers`.
-      // If the proxy server uses HTTPS, then you must set the protocol to `https`. 
-      proxy: {
-        protocol: 'https',
-        host: '127.0.0.1',
-        port: 9000,
-        auth: {
-          username: 'mikeymike',
-          password: 'rapunz3l'
-        }
-      },
-
-      // `cancelToken` specifies a cancel token that can be used to cancel the request
-      // (see Cancellation section below for details)
-      cancelToken: new CancelToken(function (cancel) {
-      }),
-
-      // `decompress` indicates whether or not the response body should be decompressed 
-      // automatically. If set to `true` will also remove the 'content-encoding' header 
-      // from the responses objects of all decompressed responses
-      // - Node only (XHR cannot turn off decompression)
-      decompress: true // default
-___________________________________________________________________________________________________
-*/
-
 
 
 /*
@@ -331,15 +105,15 @@ export const useAPI = (axiosParams) => {
     const fetchData = async (params) => {
       try {
             setRequestNum(requestNum+1);
-            log.debug(`API: executing request (${JSON.stringify(params)})`); 
+            log.debug(`API: executing request (${JSON.stringify(params)})`);
             const result = await axios.request(params);
-            log.debug(`API: request ${requestNum} successful`); 
+            log.debug(`API: request ${requestNum} successful`);
             setResponse(result.data);
        } catch( error ) {
             log.error(`API: request ${requestNum} error ${error.message}`);
             if (!unmounted) {
                 setError(error);
-            } 
+            }
        } finally {
            if (!unmounted) {
                 log.debug(`API: request ${requestNum} setting loading to false`);
